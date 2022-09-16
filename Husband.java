627568b0f806608edb158bf2ce650786aa1dfba1
unknown <raviteja.xworkz@gmail.com>
2022-09-16 15:57:30 +0530

    ravi

---
+++ Husband.java
@@ -0,0 +1,35 @@
+class Husband{
+	
+	static void me()
+	{
+		System.out.println("me");
+	}
+	static void humanBeing()
+	{
+		System.out.println("humanBeing");
+	}
+	static void men()
+	{
+		System.out.println("men");
+	}
+	static void forcedWorker()
+	{
+		System.out.println("forcedWorker");
+	}
+	static void regularCustomerToBar()
+	{
+		System.out.println("regularCustomerToBar");
+	}
+	static void careing()
+	{
+		System.out.println("careing");
+	}
+	static void protecting()
+	{
+		System.out.println("protecting");
+	}
+	static void \\()
+	{
+		System.out.println("loving");
+	}
+}
---
+++ Identity.java
@@ -0,0 +1,62 @@
+class Identity{
+	
+	static void printNames(String name)
+	{
+		System.out.println("Name :"+name);
+    }
+	static void printNameAndEmail(String Email,String Name)
+	{
+		System.out.println("Email :"+Email);
+		System.out.println("Name :"+Name);
+
+	}
+	static void printNameAndMobile(String Name,long mobile)
+	{
+				System.out.println("Name :"+Name);
+				System.out.println("Mobileno :"+mobile);
+				
+	}
+	static void printNameAndAddress(String Name,String Address)
+	{
+			System.out.println("Name :"+Name);
+			System.out.println("Address :"+Address);
+	}
+	static void printNameAndAge(String Name,int age)
+	{
+			System.out.println("Name :"+Name);
+					System.out.println("Age :"+age);
+	}
+	static void printNameAndEmailAndAddressAndAge(String Name,String Email,String Address,int age)
+	{
+		System.out.println("Email :"+Email);
+		System.out.println("Name :"+Name);
+		System.out.println("Age :"+age);
+		System.out.println("Mobileno :"+Mobile);
+	}
+	/*static void printNameAndGender(String Name,String Gender)
+	{
+		System.out.println("Name :"+Name);
+		System.out.println("Gender :"+Gender);
+	}
+	static void printNameAndWorking(String Name,boolean Workig)
+	{
+		System.out.println("Name :"+Name);
+		System.out.println("Working :"+Working);
+	}
+	static void printNameAndShirtSize(String Name,char shirtSize)
+	{
+		System.out.println("Name :"+Name);
+		System.out.println("ShirtSize :"+shirtSize);
+	}
+	static void printNameAndPantSize(String Name,int pantSize)
+	{
+		System.out.println("Name :"+Name);
+		System.out.println("PantSize :"+pantSize);
+	}
+	static void printNameAndWeightAndHeight(String Name,int weight,float height)
+	{
+		System.out.println("Name :"+Name);
+		System.out.println("Weight :"+weight);
+		System.out.println("Height :"+height);
+	}*/
+}
