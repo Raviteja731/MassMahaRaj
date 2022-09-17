9418e5c0ca1e3c8fcca1390faec7aa9106f3b416
Raviteja731 <113470312+Raviteja731@users.noreply.github.com>
2022-09-17 16:51:39 +0530

    Add files via upload

---
+++ IdentityRunner.java
@@ -0,0 +1,167 @@
+class IdentityRunner{
+
+	public static void main(String...Ra)
+	{	
+	System.out.println("My Friends");
+		Identity.printNames("Surya");
+	    Identity.printNames("Raviteja");
+		Identity.printNames("Soma");
+		Identity.printNames("Prasad");
+		Identity.printNames("Shashi");
+		Identity.printNames("Veeresh");
+		Identity.printNames("Bhadra");
+		Identity.printNames("Manoj");
+		Identity.printNames("Sharana");
+		Identity.printNames("Linga");
+		Identity.printNames("Venkatesh");
+		Identity.printNames("Basava");
+	
+	System.out.println("Name And Email");
+		Identity.printNameAndEmail("Surya","Surya@gmail.com");
+		Identity.printNameAndEmail("Raviteja","Raviteja@gmail.com");
+		Identity.printNameAndEmail("Soma","Soma@gmail.com");
+		Identity.printNameAndEmail("Prasad","Prasad@gmail.com");
+		Identity.printNameAndEmail("Shashi","Shashi@gmail.com");
+		Identity.printNameAndEmail("Veeresh","Veeresh@gmail.com");
+		Identity.printNameAndEmail("Bhadra","Bhadra@gmail.com");
+		Identity.printNameAndEmail("Manoj","Manoj@gmail.com");
+		Identity.printNameAndEmail("Sharana","Sharana@gmail.com");
+		Identity.printNameAndEmail("Linga","Linga@gmail.com");
+		Identity.printNameAndEmail("Venkatesh","Venkatesh@gmail.com");
+		Identity.printNameAndEmail("Basava","Basava@gmail.com");
+		
+		System.out.println("Name And Mobileno");
+			long ref =7026167838L;
+		 Identity.printNameAndMobile("Surya",ref);
+		 Identity.printNameAndMobile("Raviteja",ref);
+		 Identity.printNameAndMobile("Soma",ref);
+		 Identity.printNameAndMobile("Prasad",ref);
+		 Identity.printNameAndMobile("Shashi",ref);
+		 Identity.printNameAndMobile("Veeresh",ref);
+		 Identity.printNameAndMobile("Bhadra",ref);
+		 Identity.printNameAndMobile("Manoj",ref);
+		 Identity.printNameAndMobile("Sharana",ref);
+		 Identity.printNameAndMobile("Linga",ref);
+		 Identity.printNameAndMobile("Venkatesh",ref);
+		 Identity.printNameAndMobile("Basava",ref);
+		 
+		 System.out.println("Name And Address");
+		 Identity.printNameAndAddress("Surya","Bellary");
+		 Identity.printNameAndAddress("Raviteja","Hydrabad");
+		 Identity.printNameAndAddress("Soma","Banglore");
+		 Identity.printNameAndAddress("Prasad","Sandru");
+		 Identity.printNameAndAddress("Shashi","Hubbli");
+		 Identity.printNameAndAddress("Veeresh","Dharwad");
+		 Identity.printNameAndAddress("Bhadra","Koppla");
+		 Identity.printNameAndAddress("Manoj","Hospet");
+		 Identity.printNameAndAddress("Sharana","Gangavathi");
+		 Identity.printNameAndAddress("Linga","Karnool");
+		 Identity.printNameAndAddress("Venkatesh","Mumbai");
+		 Identity.printNameAndAddress("Basava","Agra");
+		 
+			byte ref1 = 23;
+		 System.out.println("printNameAndAge"); 
+		Identity.printNameAndAge("Surya",ref1);
+		 Identity.printNameAndAge("Raviteja",ref1);
+		 Identity.printNameAndAge("Soma",ref1);
+		 Identity.printNameAndAge("prasad",ref1);
+		 Identity.printNameAndAge("Shashi",ref1);
+		 Identity.printNameAndAge("Veeresh",ref1);
+		 Identity.printNameAndAge("Bhadra",ref1);
+		 Identity.printNameAndAge("Manoj",ref1);
+		 Identity.printNameAndAge("Sharana",ref1);
+		 Identity.printNameAndAge("Linga",ref1);
+		 Identity.printNameAndAge("Venkatesh",ref1);
+		 Identity.printNameAndAge("Basava",ref1);
+		 
+		 System.out.println("printNameAndEmailAndAddressAndAge");
+		 Identity.printNameAndEmailAndAddressAndAge("Surya","Surya@gmail.com","Bellary",ref1);
+		 Identity.printNameAndEmailAndAddressAndAge("Raviteja","Raviteja@gmail.com","Hydrabad",ref1);
+		 Identity.printNameAndEmailAndAddressAndAge("Soma","Soma@gmail.com","Banglore",ref1);
+		 Identity.printNameAndEmailAndAddressAndAge("Prasad","Prasad@gmail.com","Sandru",ref1);
+		 Identity.printNameAndEmailAndAddressAndAge("Shashi","Shashi@gmail.com","Hubbli",ref1);
+		 Identity.printNameAndEmailAndAddressAndAge("Veeresh","veeresh@gmail.com","Hospet",ref1);
+		 Identity.printNameAndEmailAndAddressAndAge("Bhadra","Bhadra@gmail.com","Koppla",ref1);
+		 Identity.printNameAndEmailAndAddressAndAge("Manoj","Manoj@gmail.com","Agra",ref1);
+		 Identity.printNameAndEmailAndAddressAndAge("Sharana","Sharana@gmail.com","Gangavathi",ref1);
+		 Identity.printNameAndEmailAndAddressAndAge("Linga","Linga@gmail.com","Karnool",ref1);
+		 Identity.printNameAndEmailAndAddressAndAge("Venkatesh","Venkatesh@gmail.com","Mumbai",ref1);
+		 Identity.printNameAndEmailAndAddressAndAge("Basava","Basava@gmail.com","Dharwad",ref1);
+		 
+		 System.out.println("printNameAndGender");
+		 String gender = "Male";
+		Identity.printNameAndGender("Surya",gender);
+		Identity.printNameAndGender("Raviteja",gender);
+		Identity.printNameAndGender("Soma",gender);
+		Identity.printNameAndGender("Prasad",gender);
+		Identity.printNameAndGender("Shashi",gender);
+		Identity.printNameAndGender("Sharana",gender);
+		Identity.printNameAndGender("Bhadra",gender);
+		Identity.printNameAndGender("Manoj",gender);
+		Identity.printNameAndGender("Veeresh",gender);
+		Identity.printNameAndGender("Venkatesh",gender);
+		Identity.printNameAndGender("Linga",gender);
+		Identity.printNameAndGender("Basava",gender);
+		
+		System.out.println("printNameAndWorking");
+		boolean working1 = true;
+		Identity.printNameAndWorking("Surya",working1);
+		Identity.printNameAndWorking("Raviteja",working1);
+		Identity.printNameAndWorking("Soma",working1);
+		Identity.printNameAndWorking("Shashi",working1);
+		Identity.printNameAndWorking("Sharana",working1);
+		Identity.printNameAndWorking("Prasad",working1);
+		Identity.printNameAndWorking("Bhadra",working1);
+		Identity.printNameAndWorking("Veeresh",working1);
+		Identity.printNameAndWorking("Venkatesh",working1);
+		Identity.printNameAndWorking("Veeresh",working1);
+		Identity.printNameAndWorking("Linga",working1);
+		Identity.printNameAndWorking("Basava",working1);
+		
+		System.out.println("printNameAndShirtSize");
+			byte Shirtsize = 34;
+			Identity.printNameAndShirtSize("Surya",Shirtsize);
+			Identity.printNameAndShirtSize("Raviteja",Shirtsize);
+			Identity.printNameAndShirtSize("Soma",Shirtsize);
+			Identity.printNameAndShirtSize("Shashi",Shirtsize);
+			Identity.printNameAndShirtSize("Sharana",Shirtsize);
+			Identity.printNameAndShirtSize("Venkatesh",Shirtsize);
+			Identity.printNameAndShirtSize("Veeresh",Shirtsize);
+			Identity.printNameAndShirtSize("Bhadra",Shirtsize);
+			Identity.printNameAndShirtSize("Manoj",Shirtsize);
+			Identity.printNameAndShirtSize("Basava",Shirtsize);
+			Identity.printNameAndShirtSize("Linga",Shirtsize);
+			Identity.printNameAndShirtSize("Prasad",Shirtsize);
+			
+			System.out.println("printNameAndPantSize");
+			byte Pantsize = 36;
+			       Identity.printNameAndPantSize("Surya",Pantsize);
+				   Identity.printNameAndPantSize("Raviteja",Pantsize);
+				   Identity.printNameAndPantSize("Linga",Pantsize);
+				   Identity.printNameAndPantSize("Basava",Pantsize);
+				   Identity.printNameAndPantSize("Shashi",Pantsize);
+				   Identity.printNameAndPantSize("Sharana",Pantsize);
+				   Identity.printNameAndPantSize("Soma",Pantsize);
+				   Identity.printNameAndPantSize("Veeresh",Pantsize);
+				   Identity.printNameAndPantSize("Venkatesh",Pantsize);
+				   Identity.printNameAndPantSize("Prasad",Pantsize);
+				   Identity.printNameAndPantSize("Bhadra",Pantsize);
+				   Identity.printNameAndPantSize("Manoj",Pantsize);
+				   
+				   System.out.println("printNameAndWeightAndHeight");
+					int weight1 = 65;
+					float height1 = 6.657898f;
+					   Identity.printNameAndWeightAndHeight("Surya",weight1,height1);
+					   Identity.printNameAndWeightAndHeight("Raviteja",weight1,height1);
+					   Identity.printNameAndWeightAndHeight("Bhadra",weight1,height1);
+					   Identity.printNameAndWeightAndHeight("Prasad",weight1,height1);
+					   Identity.printNameAndWeightAndHeight("Manoj",weight1,height1);
+					   Identity.printNameAndWeightAndHeight("Basava",weight1,height1);
+					   Identity.printNameAndWeightAndHeight("Soma",weight1,height1);
+					   Identity.printNameAndWeightAndHeight("Shashi",weight1,height1);
+					   Identity.printNameAndWeightAndHeight("Sharana",weight1,height1);
+					   Identity.printNameAndWeightAndHeight("Veeresh",weight1,height1);
+					   Identity.printNameAndWeightAndHeight("Venkatesh",weight1,height1);
+					   Identity.printNameAndWeightAndHeight("Linga",weight1,height1);
+	}	
+}
